e193f7482c372557a7c37f360b990c8e7f77601e
afiqkml <afiqkml@gmail.com>
2021-01-12 04:13:21 +0800

    lastest update

--- lib/screens/home/settings_form.dart
+++ lib/screens/home/settings_form.dart
@@ -36,82 +36,93 @@ class _SettingsFormState extends State<SettingsForm> {
   @override
   Widget build(BuildContext context) {
     final user = Provider.of<User>(context);
-    return StreamBuilder<UserData>(
-        stream: DatabaseService(userID: user.userID).userData,
-        builder: (context, snapshot) {
-          if (snapshot.hasData) {
-            UserData userData = snapshot.data;
+    return SingleChildScrollView(
+      child: StreamBuilder<UserData>(
+          stream: DatabaseService(userID: user.userID).userData,
+          builder: (context, snapshot) {
+            if (snapshot.hasData) {
+              UserData userData = snapshot.data;
 
-            return Form(
-              key: _formKey,
-              child: Column(
-                children: <Widget>[
-                  Text('Update order', style: TextStyle(fontSize: 18.0)),
-                  SizedBox(height: 20.0),
-                  TextFormField(
-                    initialValue: userData.name,
-                    decoration: textInputDecoration,
-                    validator: (val) => val.isEmpty ? 'Enter a name' : null,
-                    onChanged: (val) => setState(() => _currentName = val),
-                  ),
-                  SizedBox(height: 10.0),
-                  Slider(
-                    value: (_currentRice ?? userData.rice).toDouble(),
-                    activeColor: Colors.brown[_currentRice ?? userData.rice],
-                    inactiveColor: Colors.brown[_currentRice ?? userData.rice],
-                    min: 0.0,
-                    max: 500.0,
-                    divisions: 5,
-                    onChanged: (val) =>
-                        setState(() => _currentRice = val.round()),
-                  ),
-                  SizedBox(height: 10.0),
-                  DropdownButtonFormField(
-                    decoration: textInputDecoration,
-                    value: _currentDish ?? userData.dish,
-                    items: dishes.map((dish) {
-                      return DropdownMenuItem(
-                        value: dish,
-                        child: Text('$dish'),
-                      );
-                    }).toList(),
-                    onChanged: (val) => setState(() => _currentDish = val),
-                  ),
-                  SizedBox(height: 10.0),
-                  DropdownButtonFormField(
-                    decoration: textInputDecoration,
-                    value: _currentSides ?? 'No sides',
-                    items: sides.map((side) {
-                      return DropdownMenuItem(
-                        value: side,
-                        child: Text('$side'),
-                      );
-                    }).toList(),
-                    onChanged: (val) => setState(() => _currentSides = val),
-                  ),
-                  SizedBox(height: 10.0),
-                  RaisedButton(
-                    color: Colors.pink[400],
-                    child:
-                        Text('Update', style: TextStyle(color: Colors.white)),
-                    onPressed: () async {
-                      if (_formKey.currentState.validate()) {
-                        await DatabaseService(userID: user.userID)
-                            .updateUserData(
-                                _currentName ?? userData.name,
-                                _currentRice ?? userData.rice,
-                                _currentDish ?? userData.dish,
-                                _currentSides ?? userData.sides);
-                      }
-                      ;
-                    },
-                  )
-                ],
-              ),
-            );
-          } else {
-            return Loading();
-          }
-        });
+              return Form(
+                key: _formKey,
+                child: Column(
+                  children: <Widget>[
+                    Text('Update order', style: TextStyle(fontSize: 18.0)),
+                    SizedBox(height: 20.0),
+                    TextFormField(
+                      initialValue: userData.name,
+                      decoration: InputDecoration(
+                        labelText: 'Customer Name',
+                      ),
+                      validator: (val) => val.isEmpty ? 'Enter a name' : null,
+                      onChanged: (val) => setState(() => _currentName = val),
+                    ),
+                    SizedBox(height: 10.0),
+                    Text('Amount of Rice', style: TextStyle(fontSize: 15.0)),
+                    Slider(
+                      label: _currentRice.toString(),
+                      value: (_currentRice ?? userData.rice).toDouble(),
+                      activeColor: Colors.brown[_currentRice ?? userData.rice],
+                      inactiveColor:
+                          Colors.brown[_currentRice ?? userData.rice],
+                      min: 0.0,
+                      max: 500.0,
+                      divisions: 5,
+                      onChanged: (val) =>
+                          setState(() => _currentRice = val.round()),
+                    ),
+                    SizedBox(height: 10.0),
+                    DropdownButtonFormField(
+                      decoration: InputDecoration(
+                        labelText: 'Side Dish',
+                      ),
+                      value: _currentDish ?? userData.dish,
+                      items: dishes.map((dish) {
+                        return DropdownMenuItem(
+                          value: dish,
+                          child: Text('$dish'),
+                        );
+                      }).toList(),
+                      onChanged: (val) => setState(() => _currentDish = val),
+                    ),
+                    SizedBox(height: 10.0),
+                    DropdownButtonFormField(
+                      decoration: InputDecoration(
+                        labelText: 'Condiments',
+                      ),
+                      value: _currentSides ?? 'No sides',
+                      items: sides.map((side) {
+                        return DropdownMenuItem(
+                          value: side,
+                          child: Text('$side'),
+                        );
+                      }).toList(),
+                      onChanged: (val) => setState(() => _currentSides = val),
+                    ),
+                    SizedBox(height: 10.0),
+                    RaisedButton(
+                      color: Colors.pink[400],
+                      child:
+                          Text('Update', style: TextStyle(color: Colors.white)),
+                      onPressed: () async {
+                        if (_formKey.currentState.validate()) {
+                          await DatabaseService(userID: user.userID)
+                              .updateUserData(
+                                  _currentName ?? userData.name,
+                                  _currentRice ?? userData.rice,
+                                  _currentDish ?? userData.dish,
+                                  _currentSides ?? userData.sides);
+                        }
+                        ;
+                      },
+                    )
+                  ],
+                ),
+              );
+            } else {
+              return Loading();
+            }
+          }),
+    );
   }
 }
